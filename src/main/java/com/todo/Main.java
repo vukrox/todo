9ec5a7138310782a556789326f15d4289d9649bf
vukrox <62335798+vukrox@users.noreply.github.com>
2020-11-07 11:29:43 +0300

    Deleted: 1. Engine 2.AccRepFileStorage 3.Cleaned main method

--- src/main/java/com/todo/Main.java
+++ src/main/java/com/todo/Main.java
@@ -1,23 +1,7 @@
 package com.todo;
 
-import com.todo.domain.Account;
-import com.todo.engine.AccountEngine;
-import com.todo.repository.AccountRepository;
-import com.todo.repository.AccountRepositoryFileStorage;
-import com.todo.service.AccountService;
-import com.todo.service.AccountServiceImpl;
-
 public class Main {
     public static void main(String[] args) {
-        AccountRepository repository = new AccountRepositoryFileStorage();
-
-        AccountService service = new AccountServiceImpl(repository);
-
-        AccountEngine engine = new AccountEngine(service);
-
-        Account account = engine.displayMainMenu();
-
-        engine.displayUserMenu(account);
 
     }
 }
